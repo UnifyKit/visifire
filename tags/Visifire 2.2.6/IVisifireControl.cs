eb0dadc620d87f0d3f19ebe354c75dff4227a2f6
somnathpanja <somnathpanja@54de0435-1549-0410-a7aa-c78141fa7197>
2009-08-21 14:07:02 +0000

    Visifire 2.2.6

---
+++ tags/Visifire 2.2.6/IVisifireControl.cs
@@ -0,0 +1,66 @@
+﻿#if WPF
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Markup;
+using System.IO;
+using System.Xml;
+using System.Threading;
+using System.Windows.Automation.Peers;
+using System.Windows.Automation;
+using System.Globalization;
+using System.Diagnostics;
+using System.Collections.ObjectModel;
+using System.Windows.Threading;
+#else
+using System;
+using System.Windows;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+using System.Windows.Markup;
+using System.Collections.ObjectModel;
+
+#endif
+
+namespace Visifire.Commons
+{   
+    /// <summary>
+    /// Visifire Control interface
+    /// </summary>
+    public abstract class VisifireControl: Control
+    {   
+        /// <summary>
+        /// Root visual element of the chart control 
+        /// </summary>
+        public Grid RootElement { get; set;  }
+
+        /// <summary>
+        /// Flag if template is already applied
+        /// </summary>
+        Boolean IsTemplateApplied { get; set; }
+
+        /// <summary>
+        /// ToolTip object
+        /// </summary>
+        internal Border ToolTipElement { get; set; }
+    }
+
+}
---
+++ tags/Visifire 2.2.6/ReadMe.txt
@@ -0,0 +1,6 @@
+Follow the steps required for setting up an environment for compiling and running Visifire source
+
+1) Install Visual studio 2008 with SP1
+2) Install Silverlight 3.0 Tools for Visual Studio 2008 SP1.
+3) You are done.
+4) Now you can run the source code of Visifire.
