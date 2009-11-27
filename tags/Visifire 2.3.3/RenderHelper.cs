d24f8b63893e6cf85999c0d0c108060e2e026d41
somnathpanja <somnathpanja@54de0435-1549-0410-a7aa-c78141fa7197>
2009-11-27 12:56:06 +0000

    Visifire Silverlight and WPF Charts v2.3.3 Released.

---
+++ tags/Visifire 2.3.3/RenderHelper.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+
+namespace Visifire.Charts
+{
+    public static class RenderHelper
+    {
+        internal static Panel GetVisualObject(RenderAs chartType, Double width, Double height, PlotDetails plotDetails, List<DataSeries> dataSeriesList4Rendering, Chart chart, Double plankDepth, bool animationEnabled)
+        {
+            Panel renderedCanvas = null;
+
+            switch (chartType)
+            {
+                case RenderAs.Column:
+                    renderedCanvas = ColumnChart.GetVisualObjectForColumnChart(width, height, plotDetails, dataSeriesList4Rendering, chart, plankDepth, animationEnabled);
+                    break;
+
+                case RenderAs.Bar:
+                    renderedCanvas = BarChart.GetVisualObjectForBarChart(width, height, plotDetails, dataSeriesList4Rendering, chart, plankDepth, animationEnabled);
+                    break;
+
+                case RenderAs.Line:
+                    renderedCanvas = LineChart.GetVisualObjectForLineChart(width, height, plotDetails, dataSeriesList4Rendering, chart, plankDepth, animationEnabled);
+                    break;
+
+                case RenderAs.Point:
+                    renderedCanvas = PointChart.GetVisualObjectForPointChart(width, height, plotDetails, dataSeriesList4Rendering, chart, plankDepth, animationEnabled);
+                    break;
+
+                case RenderAs.Bubble:
+                    renderedCanvas = BubbleChart.GetVisualObjectForBubbleChart(width, height, plotDetails, dataSeriesList4Rendering, chart, plankDepth, animationEnabled);
+                    break;
+
+                case RenderAs.Area:
+                    renderedCanvas = AreaChart.GetVisualObjectForAreaChart(width, height, plotDetails, dataSeriesList4Rendering, chart, plankDepth, animationEnabled);
+                    break;
+
+                case RenderAs.StackedColumn:
+                    renderedCanvas = ColumnChart.GetVisualObjectForStackedColumnChart(width, height, plotDetails, chart, plankDepth, animationEnabled);
+                    break;
+
+                case RenderAs.StackedColumn100:
+                    renderedCanvas = ColumnChart.GetVisualObjectForStackedColumn100Chart(width, height, plotDetails, chart, plankDepth, animationEnabled);
+                    break;
+
+                case RenderAs.StackedBar:
+                    renderedCanvas = BarChart.GetVisualObjectForStackedBarChart(width, height, plotDetails, chart, plankDepth, animationEnabled);
+                    break;
+
+                case RenderAs.StackedBar100:
+                    renderedCanvas = BarChart.GetVisualObjectForStackedBar100Chart(width, height, plotDetails, chart, plankDepth, animationEnabled);
+                    break;
+
+                case RenderAs.Pie:
+                    renderedCanvas = PieChart.GetVisualObjectForPieChart(width, height, plotDetails, dataSeriesList4Rendering, chart, animationEnabled);
+                    break;
+
+                case RenderAs.Doughnut:
+                    renderedCanvas = PieChart.GetVisualObjectForDoughnutChart(width, height, plotDetails, dataSeriesList4Rendering, chart, animationEnabled);
+                    break;
+
+                case RenderAs.StackedArea:
+                    renderedCanvas = AreaChart.GetVisualObjectForStackedAreaChart(width, height, plotDetails, dataSeriesList4Rendering, chart, plankDepth, animationEnabled);
+                    break;
+
+                case RenderAs.StackedArea100:
+                    renderedCanvas = AreaChart.GetVisualObjectForStackedArea100Chart(width, height, plotDetails, dataSeriesList4Rendering, chart, plankDepth, animationEnabled);
+                    break;
+
+                case RenderAs.SectionFunnel:
+                    renderedCanvas = FunnelChart.GetVisualObjectForFunnelChart(width, height, plotDetails, dataSeriesList4Rendering, chart, animationEnabled, false);
+
+                    break;
+
+                case RenderAs.StreamLineFunnel:
+                    renderedCanvas = FunnelChart.GetVisualObjectForFunnelChart(width, height, plotDetails, dataSeriesList4Rendering, chart, animationEnabled, true);
+                    break;
+
+                case RenderAs.Stock:
+                    renderedCanvas = StockChart.GetVisualObjectForStockChart(width, height, plotDetails, dataSeriesList4Rendering, chart, plankDepth, animationEnabled);
+                    break;
+
+                case RenderAs.CandleStick:
+                    renderedCanvas = CandleStick.GetVisualObjectForCandleStick(width, height, plotDetails, dataSeriesList4Rendering, chart, plankDepth, animationEnabled);
+                    break;
+            }
+
+            return renderedCanvas;
+        }
+
+    }
+}
---
+++ tags/Visifire 2.3.3/SLVisifireChartsXap/Page.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Visifire.Charts;
+
+namespace SLVisifireChartsXap
+{
+    public partial class Page : UserControl
+    {
+        public Page()
+        {
+            InitializeComponent();
+            //Button1.Click += new RoutedEventHandler(Button1_Click);
+
+            c.Height = 300;
+            c.Width = 500;
+            c.Background = new SolidColorBrush(Colors.Green);
+
+            // remove from here
+            Random rand = new Random();
+            for (Int32 i = 0; i < 2; i++)
+            {
+                DataSeries ds = new DataSeries();
+                for (Int32 j = 0; j < 10; j++)
+                {
+                    DataPoint dp = new DataPoint();
+                    dp.XValue = j + 1;
+                    dp.YValue = rand.Next(-1000, 0);
+                    dp.ZValue = rand.Next(0, 100);
+                    dp.AxisXLabel = "XValue Axis Labels " + dp.XValue.ToString();
+                    ds.DataPoints.Add(dp);
+                }
+                ds.RenderAs = RenderAs.Column;
+                ds.AxisXType = AxisTypes.Primary;
+                ds.AxisYType = AxisTypes.Primary;
+
+                ds.ZIndex = rand.Next(0, 50);
+                c.Series.Add(ds);
+            }
+
+            // remove till here
+
+            Title t = new Title("Title1");
+            t.FontColor = new SolidColorBrush(Colors.Red);
+            t.VerticalAlignment = VerticalAlignment.Center;
+            t.HorizontalAlignment = HorizontalAlignment.Left;
+            c.Titles.Add(t);
+            t.FontColor = new SolidColorBrush(Colors.Yellow);
+            this.LayoutRoot.Children.Add(c);
+            LayoutRoot.MouseMove += new MouseEventHandler(LayoutRoot_MouseMove);
+            LayoutRoot.Children.Add(tx);
+        }
+
+        void LayoutRoot_MouseMove(object sender, MouseEventArgs e)
+        {
+            //tx.Text = e.GetPosition(c).ToString();
+        }
+
+        void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            Title t = new Title("Title1");
+            t.FontColor = new SolidColorBrush(Colors.Red);
+            t.VerticalAlignment = VerticalAlignment.Center;
+            t.HorizontalAlignment = HorizontalAlignment.Left;
+            c.Titles.Add(t);
+            t.FontColor = new SolidColorBrush(Colors.Yellow);
+        }
+
+        Chart c = new Chart();
+        TextBlock tx = new TextBlock() { Foreground = new SolidColorBrush(Colors.Black)};
+    }
+}
